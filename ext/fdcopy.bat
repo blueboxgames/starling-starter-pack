10ea14d4083fe1b43862a9ed2311753999e89843
FudoSakura <fudo.sakurai@gmail.com>
2019-04-25 16:23:43 +0430

    FD shortcut fix + C:\AIR32 Setting copy

--- ext/fdcopy.bat
+++ ext/fdcopy.bat
@@ -1,6 +1,6 @@
 @echo off
 echo Set oWS = WScript.CreateObject("WScript.Shell") > CreateShortcut.vbs
-echo sLinkFile = "%APPDATA%\Roaming\Microsoft\Windows\Start Menu\Programs" >> CreateShortcut.vbs
+echo sLinkFile = "%APPDATA%\Microsoft\Windows\Start Menu\Programs\FlashDevelop.lnk" >> CreateShortcut.vbs
 echo Set oLink = oWS.CreateShortcut(sLinkFile) >> CreateShortcut.vbs
 echo oLink.TargetPath = "C:\Program Files (x86)\FlashDevelop\FlashDevelop64.exe" >> CreateShortcut.vbs
 echo oLink.Save >> CreateShortcut.vbs
